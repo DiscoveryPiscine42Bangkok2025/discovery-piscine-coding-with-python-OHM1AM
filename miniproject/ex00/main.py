5e5d469eceda7a09812655c2b54500eb16a91270
OHM1AM <ohmchanuwat1am@gmail.com>
2025-09-18 22:07:39 +0700

    add check err

--- miniproject/ex00/main.py
+++ miniproject/ex00/main.py
@@ -27,6 +27,26 @@ R...
 ....P...
 """
     checkmate(board3)   #For TEST
+    
+    #For TEST Err1
+    board4 = 1
+
+    checkmate(board4) #For TEST
+
+    #For TEST Err2
+    board5 = """\
+"""
+    checkmate(board5)   #For TEST
+
+    #For TEST Err3
+    board6 = """\
+........
+........
+...K....
+....P...
+"""
+    checkmate(board6)   #For TEST
+
 
 if __name__ == "__main__":
     main()
